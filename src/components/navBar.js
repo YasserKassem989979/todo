bd92aaabb228c227c6bdb4fe9782c67d7bcc1cda
yasserkassem <eng.yasserkassem@gmail.com>
2021-01-01 17:19:04 +0200

    navbar

---
+++ src/components/navBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    useLocation,
+    Link
+} from "react-router-dom";
+const NavBar = () => {
+    const location = useLocation();
+    return (
+        <ul className="nav nav-pills justify-content-center">
+            <li className={`nav-item`}>
+                <Link to="/" className={`nav-link ${location.pathname === "/" ? " active " : ""}`}>Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/calendar" className={`nav-link ${location.pathname === "/calendar" ? " active " : ""}`}>Calendar</Link>
+            </li>
+        </ul>
+    )
+}
+
+export default NavBar
